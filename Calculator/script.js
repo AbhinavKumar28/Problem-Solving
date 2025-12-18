de2f2023923e4bb89a52922b4cce90ec4006f40f
Abhinav Kumar <emailofabhinav@gmail.com>
2025-12-18 17:36:49 +0530

    feat: implemented button functionality

--- Calculator/script.js
+++ Calculator/script.js
@@ -23,3 +23,73 @@ const operate = (operator, num1,num2)=>{
         console.log("Invalid operator")
     }
 }
+let a = ""
+let b= ""
+let op=""
+let flag =0
+let sign = "+"
+const mainInputElement = document.getElementById("mainInput")
+const onClickNum=(num)=>{
+    if (flag===0){
+        a=a+num
+        mainInputElement.value=a
+    }else {
+        b=b+num
+        mainInputElement.value=b
+    }
+}
+const onClickOp=(oper)=>{
+    if (a!==""){
+        op=oper
+        flag=1
+        mainInputElement.value=""
+        b=""
+    }
+}
+const onClickDot = ()=>{
+    if (flag===0){
+        if (a.includes(".")){
+            return
+        }else {
+            a=a+"."
+            mainInputElement.value=a
+        }
+    }else {
+        if (b.includes(".")){
+            return
+        }else {
+            b=b+"."
+            mainInputElement.value=b
+        }
+    }
+}
+const onClickClear=()=>{
+    a=""
+    b=""
+    op=""
+    flag=0
+    mainInputElement.value=""
+}
+const onClickSign=()=>{
+    if (sign==="+"){
+        sign="-"
+        if (flag===0){
+            a="-"+a
+            mainInputElement.value=a
+        }else {
+            b="-"+b
+            mainInputElement.value=b
+        }
+    }else {
+        sign="+"
+        if (flag===0){
+            let newstring=a.slice(1)
+            a=newstring
+            mainInputElement.value=a
+        }else {
+            let newstring=b.slice(1)
+            b=newstring
+            mainInputElement.value=b
+        }
+    }
+}
