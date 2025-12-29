d3fcb27a92c8f5171763fa5503a057394b1a7726
Abhinav Kumar <emailofabhinav@gmail.com>
2025-12-29 20:57:37 +0530

    wip: grid another project incomplete

--- react-form-project/src/components/Form.jsx
+++ react-form-project/src/components/Form.jsx
@@ -1,18 +1,7 @@
-// const inputdata= [
-//     {"required":true, "type":"text", "name":"firstname"},
-// {"name":"lastname","type":"text","required":false,},
-//     {"required":true, "type":"email", "name":"email"},
-// {"name":"phonenumber","type":"tel","required":true,},
-//     {"required":true, "type":"password", "name":"password"},
-// {"name":"confirmpassword","type":"password","required":true}
-// ]
+
 function Form({index,inpobj}){
     console.log(inpobj.errormessage)
     return (
-        // <div class="inputs">
-        //     <label for="fname">FIRST NAME*</label>
-        //     <input minlength="3" maxlength="18" required type="text" name="firstname" id="fname"/>
-        // </div>
         <div key={index} className="inputs">
             <label htmlFor={inpobj.name}>{inpobj.name.toUpperCase()}{inpobj.required?"*":""}</label>
             <input pattern={((inpobj.name==="password")||(inpobj.name==="confirmpassword"))?"(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}":undefined}
