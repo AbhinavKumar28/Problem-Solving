4e26f6a6e92590db5abcdf20acc3623ede03900c
Abhinav Kumar <emailofabhinav@gmail.com>
2026-01-23 16:49:14 +0530

    feat: part 1 half completed

---
+++ FullStackOpen/part1/1-1&1-2/src/components/Total.jsx
@@ -0,0 +1,8 @@
+const Total = ({parts}) => {
+  return (
+    <>
+      <p>Number of exercises {parts[0].exercises + parts[0].exercises + parts[0].exercises}</p>
+    </>
+  )
+}
+export default Total
